dab95bdfb77a8dcd148d5487dcbfe98d7af018c2
mm <asreweb81@gmail.com>
2021-01-16 19:35:57 +0330

    Initial commit

---
+++ ShutdownTimerForm/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace ShutdownTimerForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtHour = new System.Windows.Forms.TextBox();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.txtMinute = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnStart.Location = new System.Drawing.Point(179, 188);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(98, 35);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Location = new System.Drawing.Point(4, 188);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(106, 35);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtHour
+            // 
+            this.txtHour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtHour.Location = new System.Drawing.Point(156, 15);
+            this.txtHour.Name = "txtHour";
+            this.txtHour.Size = new System.Drawing.Size(78, 20);
+            this.txtHour.TabIndex = 2;
+            this.txtHour.Text = " ";
+            this.txtHour.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtHour.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.BackColor = System.Drawing.Color.Cyan;
+            this.txtSecond.Location = new System.Drawing.Point(156, 91);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(78, 20);
+            this.txtSecond.TabIndex = 3;
+            this.txtSecond.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSecond.TextChanged += new System.EventHandler(this.txtSecond_TextChanged);
+            // 
+            // txtMinute
+            // 
+            this.txtMinute.BackColor = System.Drawing.Color.Cyan;
+            this.txtMinute.Location = new System.Drawing.Point(156, 54);
+            this.txtMinute.Name = "txtMinute";
+            this.txtMinute.Size = new System.Drawing.Size(78, 20);
+            this.txtMinute.TabIndex = 4;
+            this.txtMinute.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtMinute.TextChanged += new System.EventHandler(this.txtMinute_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Hour";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Minute";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(240, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Second";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(240, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Status";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.txtStatus.Enabled = false;
+            this.txtStatus.Location = new System.Drawing.Point(12, 122);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.Size = new System.Drawing.Size(222, 20);
+            this.txtStatus.TabIndex = 9;
+            this.txtStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtStatus.TextChanged += new System.EventHandler(this.txtStatus_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(97, 18);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(29, 13);
+            this.lblYear.TabIndex = 10;
+            this.lblYear.Text = "Year";
+            // 
+            // lblMonth
+            // 
+            this.lblMonth.AutoSize = true;
+            this.lblMonth.Location = new System.Drawing.Point(97, 57);
+            this.lblMonth.Name = "lblMonth";
+            this.lblMonth.Size = new System.Drawing.Size(37, 13);
+            this.lblMonth.TabIndex = 11;
+            this.lblMonth.Text = "Month";
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Location = new System.Drawing.Point(97, 94);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(26, 13);
+            this.lblDay.TabIndex = 12;
+            this.lblDay.Text = "Day";
+            // 
+            // txtYear
+            // 
+            this.txtYear.BackColor = System.Drawing.Color.Cyan;
+            this.txtYear.Location = new System.Drawing.Point(12, 16);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(79, 20);
+            this.txtYear.TabIndex = 13;
+            this.txtYear.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.BackColor = System.Drawing.Color.Cyan;
+            this.txtMonth.Location = new System.Drawing.Point(12, 54);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(79, 20);
+            this.txtMonth.TabIndex = 14;
+            this.txtMonth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDay
+            // 
+            this.txtDay.BackColor = System.Drawing.Color.Cyan;
+            this.txtDay.Location = new System.Drawing.Point(12, 91);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(79, 20);
+            this.txtDay.TabIndex = 15;
+            this.txtDay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 156);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(137, 17);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "Use The Currently Date";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // txtTime
+            // 
+            this.txtTime.Enabled = false;
+            this.txtTime.Location = new System.Drawing.Point(165, 154);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(71, 20);
+            this.txtTime.TabIndex = 17;
+            this.txtTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtTime.TextChanged += new System.EventHandler(this.txtTime_TextChanged);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(248, 157);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(30, 13);
+            this.lblTime.TabIndex = 18;
+            this.lblTime.Text = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 235);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblMonth);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMinute);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.txtHour);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "ShutdownTimer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtHour;
+        private System.Windows.Forms.TextBox txtSecond;
+        private System.Windows.Forms.TextBox txtMinute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblMonth;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.TextBox txtDay;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ ShutdownTimerForm/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Globalization;
+
+namespace ShutdownTimerForm
+{
+    public partial class Form1 : Form
+    {
+        //Value
+        int HourInput;
+        int MinuteInput;
+        int SecondInput;
+        string strUserDate = null;
+        string strSystemDate = null;
+        int YearInput;
+        int MonthInput;
+        int DayInput;
+        Thread objThread2;
+        DateTime userDate;
+        Thread objThread3;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+        }
+
+        //Button Start Method
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            txtStatus.Text = "Running";
+
+            //Input Value
+            HourInput = Convert.ToInt32(txtHour.Text);
+            MinuteInput = Convert.ToInt32(txtMinute.Text);
+            SecondInput = Convert.ToInt32(txtSecond.Text);
+            YearInput = Convert.ToInt32(txtYear.Text);
+            MonthInput = Convert.ToInt32(txtMonth.Text);
+            DayInput = Convert.ToInt32(txtDay.Text);
+
+
+
+            //Analys Input Value and Convert to String
+            userDate = new DateTime(YearInput, MonthInput, DayInput, HourInput, MinuteInput, SecondInput);
+            strUserDate = userDate.ToUniversalTime().ToString("u");
+
+
+            //start New Thread or Start Timer
+            
+            
+            
+            objThread2 = new Thread(Thread2);
+            objThread2.Start();
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMinute_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSecond_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void txtStatus_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //new Thread(Method)
+        public void Thread2()
+        {
+            //Checking System Date and check systemDate == strUserDate
+            while (true)
+            {
+                DateTime dateNow = DateTime.Now;
+                string systemDate = dateNow.ToUniversalTime().ToString("u");
+
+
+                if (systemDate == strUserDate)
+                {
+                    MessageBox.Show("shutdowning...");
+                    Thread.Sleep(6000);
+                    //shutdown System
+                    System.Diagnostics.Process.Start("shutdown", "/s /t 0");
+                    
+
+
+                }
+
+                Thread.Sleep(1000);
+            }
+
+        }
+        
+        
+
+        //Timer 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            var strSystemDate = DateTime.Now.ToString("HH:mm:ss");
+            txtTime.Text = strSystemDate;
+        }
+
+
+
+        //Button Stop Method
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            //Stop Timer
+            //if (btnStart.Enabled == true)
+            //{
+            //    MessageBox.Show("Pleas Start a Progress than Stop");
+            //}
+            //else if (btnStart.Enabled == false)
+            //{
+            //    timer1.Stop();
+            //    btnStart.Enabled = true;
+            //txtStatus.Text = "Stoped";
+            //}
+
+            //Stop Thread
+            if (btnStart.Enabled == true)
+            {
+                MessageBox.Show("Pleas Start a Progress than Stop");
+
+            }
+            else if (btnStart.Enabled == false)
+            {
+                objThread2.Abort();
+                btnStart.Enabled = true;
+                txtStatus.Text = "Stoped";
+            }
+
+
+        }
+
+        //CheckBox Method
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+
+            if (checkBox1.Checked)
+            {
+
+
+                int Year = Convert.ToInt32(DateTime.Now.ToString("yyyy"));
+                int Month = Convert.ToInt32(DateTime.Now.ToString("MM"));
+                int Day = Convert.ToInt32(DateTime.Now.ToString("dd"));
+
+                PersianCalendar pc = new PersianCalendar();
+                DateTime convertShamsiToGhamari = new DateTime(Year, Month, Day, pc);
+
+                txtYear.Text = convertShamsiToGhamari.Year.ToString();
+                txtMonth.Text = convertShamsiToGhamari.Month.ToString();
+                txtDay.Text = convertShamsiToGhamari.Day.ToString();
+            }
+            else
+            {
+                txtYear.Text = null;
+                txtMonth.Text = null;
+                txtDay.Text = null;
+            }
+        }
+
+        private void txtTime_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
